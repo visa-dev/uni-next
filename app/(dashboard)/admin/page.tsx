736098059a34d1fb7c4a69f17d7538e167322765
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2025-01-05 15:22:27 +0530

    add some components for admin dashboard

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,6 +1,48 @@
+import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
+import AttendanceChart from "@/components/AttendanceChart";
+import RepeatAndMedicalCount from "@/components/RepeatAndMedicalCount";
+import EventCalender from "@/components/EventCalender";
+
+
 const AdminPage =() =>{
     return (
-        <div className=''>Admin Page</div>
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+           {/*LEFT*/}
+            <div className='w-full lg:w-2/3 flex flex-col gap-8'>
+                {/*USER CARDS*/}
+                <div className='flex justify-between gap-4 flex-wrap'>
+                    <UserCard type='student'/>
+                    <UserCard type='lecture'/>
+                </div>
+
+                {/*MIDDLE CHARTS*/}
+                <div className='flex gap-4 flex-col lg:flex-row'>
+                    {/*GENDER COUNT CHART*/}
+                   <div className='w-full lg:w-1/3 h-[450px]'>
+                       <CountChart/>
+                   </div>
+
+                    {/*ATTENDANCE CHART*/}
+                    <div className='flex lg:w-2/3 h-[450px]'>
+                        <AttendanceChart/>
+                    </div>
+                </div>
+
+                {/*BOTTOM CHARTS*/}
+                <div className='w-full h-[500px]'>
+                        <RepeatAndMedicalCount/>
+                </div>
+
+            </div>
+
+            {/*RIGHT*/}
+            <div className='w-full lg:w-1/3 '>
+                <div className='flex flex-col gap-4'>
+                    <EventCalender/>
+                </div>
+            </div>
+        </div>
     )
 }
 export default AdminPage;
---
+++ components/AttendanceChart.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import Image from "next/image";
+
+const data = [
+    {
+        name: 'Mon',
+        present: 40,
+        absent: 20,
+    },
+    {
+        name: 'Tue',
+        present: 20,
+        absent: 240,
+    },
+    {
+        name: 'Wen',
+        present: 460,
+        absent: 240,
+    },
+    {
+        name: 'Thu',
+        present: 200,
+        absent: 20,
+    },
+    {
+        name: 'Fri',
+        present: 40,
+        absent: 240,
+    },
+
+];
+
+const AttendanceChart = () => {
+    return(
+        <div className='bg-white rounded-lg w-full h-full p-4 '>
+            {/*TITLE*/}
+            <div className='flex justify-between items-center'>
+                <h1 className='text-lg font-semibold'>Attendance</h1>
+                <Image src='/moreDark.png' alt='student' width={20} height={20} />
+            </div>
+            {/*CHART*/}
+            <div className='w-full h-[95%]'>
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#ddd' />
+                        <XAxis dataKey="name" axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
+                        <YAxis axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
+                        <Tooltip contentStyle={{borderRadius:"10px" ,borderColor:"lightgrey"}} />
+                        <Legend align='left' verticalAlign='top' wrapperStyle={{paddingTop:"20px",paddingBottom:"40px"}}  />
+                        <Bar dataKey="present" fill="#8884d8"  legendType='circle' radius={[10,10,0,0]}/>
+                        <Bar dataKey="absent" fill="#82ca9d" legendType='circle' radius={[10,10,0,0]}/>
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+
+        </div>
+    )
+}
+
+export default AttendanceChart;
