fcf93215f6046b6957180b01abd15df26640d744
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2025-01-09 20:08:56 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
       <div>
         <h1 className="text-3xl font-bold underline">
-          UNI_NEXT Visa DEV
+          UNI_NEXT You WON
         </h1>
       </div>
   );
